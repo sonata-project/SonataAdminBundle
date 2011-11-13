3b046749317b8beda6950aaf678510b6d3ff4e58
Thomas Rabaix <thomas.rabaix@ekino.com>
2011-11-13 23:28:08 +0100

    [Test] Tweak test

--- Tests/tests/vendors.php
+++ Tests/tests/vendors.php
@@ -3,15 +3,14 @@
 
 set_time_limit(0);
 
-
 $vendorDir = __DIR__.'/../../vendor';
 if (!is_dir($vendorDir)) {
   mkdir($vendorDir);
 }
 
 $deps = array(
-    array('symfony', 'git://github.com/symfony/symfony.git', 'v2.0.5'),
-    array('knpmenu', 'git://github.com/knplabs/KnpMenu.git', 'master')
+    array('symfony', 'git://github.com/symfony/symfony.git', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
+    array('knpmenu', 'git://github.com/knplabs/KnpMenu.git', 'origin/master')
 );
 
 foreach ($deps as $dep) {
@@ -21,7 +20,7 @@ foreach ($deps as $dep) {
 
     $installDir = $vendorDir.'/'.$name;
     if (!is_dir($installDir)) {
-        system(sprintf('git clone %s %s', escapeshellarg($url), escapeshellarg($installDir)));
+        system(sprintf('git clone --quiet %s %s', escapeshellarg($url), escapeshellarg($installDir)));
     }
 
     system(sprintf('cd %s && git fetch origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
