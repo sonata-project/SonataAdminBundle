147ffebdede7ae882ba174b9fa0e1bf9413243d0
Matthieu Bontemps <matthieu.bontemps@knplabs.com>
2011-12-04 14:52:53 +0100

    Fix KnpLabs case in repo urls

--- Tests/tests/vendors.php
+++ Tests/tests/vendors.php
@@ -10,7 +10,7 @@ if (!is_dir($vendorDir)) {
 
 $deps = array(
     array('symfony', 'git://github.com/symfony/symfony.git', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
-    array('knpmenu', 'git://github.com/knplabs/KnpMenu.git', 'origin/master')
+    array('knpmenu', 'git://github.com/KnpLabs/KnpMenu.git', 'origin/master')
 );
 
 foreach ($deps as $dep) {
