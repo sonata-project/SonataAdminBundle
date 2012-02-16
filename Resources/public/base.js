7b4c6bde33f03dfc8048b928eb30e2c3ae95a575
Benoit Pointet <benoit.pointet@liip.ch>
2012-02-16 21:03:03 +0100

    pursued adaptations

--- Resources/public/base.js
+++ Resources/public/base.js
@@ -1,11 +1,12 @@
 jQuery(document).ready(function() {
     jQuery('html').removeClass('no-js');
-    Admin.add_pretty_errors(document);
     $('.dropdown-toggle').dropdown();
-    Admin.add_collapsed_toggle();
-    Admin.add_filters(document);
-    Admin.set_object_field_value(document);
-    Admin.setup_collection_buttons(document);
+    $(".collapse").collapse();
+    //Admin.add_pretty_errors(document);
+    //Admin.add_collapsed_toggle();
+    //Admin.add_filters(document);
+    //Admin.set_object_field_value(document);
+    //Admin.setup_collection_buttons(document);
 });
 
 var Admin = {
