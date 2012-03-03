5cb6b28e5405aa4a6e7f7987314a2f459db7a99b
Fran Moreno <franmomu@gmail.com>
2012-03-03 23:36:37 +0100

    Updated filters style

--- Resources/public/base.js
+++ Resources/public/base.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
     jQuery('html').removeClass('no-js');
     $('.dropdown-toggle').dropdown();
-    $(".collapse").collapse();
+    //$(".collapse").collapse();
     //Admin.add_pretty_errors(document);
     //Admin.add_collapsed_toggle();
     //Admin.add_filters(document);
