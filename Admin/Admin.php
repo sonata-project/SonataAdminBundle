fdda9c98c4f11e61c012af0ae03aa6f9e3a0668a
suncat2000 <n.ivlev@ailove.ru>
2012-03-05 18:28:21 +0400

    add association mappings to export

--- Admin/Admin.php
+++ Admin/Admin.php
@@ -463,6 +463,21 @@ abstract class Admin implements AdminInterface, DomainObjectInterface
     {
         return $this->getModelManager()->getExportFields($this->getClass());
     }
+    
+    /**
+     * @return array
+     */
+    public function getExportAssociationMappings()
+    {
+        $modelManager = $this->getModelManager();
+        $associationMappings = array();
+        
+        if(method_exists($modelManager, 'getExportAssociationMappings')){
+            $associationMappings = $modelManager->getExportAssociationMappings($this->getClass());
+        }
+        
+        return $associationMappings;
+    }
 
     /**
      * @return
@@ -472,7 +487,7 @@ abstract class Admin implements AdminInterface, DomainObjectInterface
         $datagrid = $this->getDatagrid();
         $datagrid->buildPager();
 
-        return $this->getModelManager()->getDataSourceIterator($datagrid, $this->getExportFields());
+        return $this->getModelManager()->getDataSourceIterator($datagrid, $this->getExportFields(), null, null, $this->getExportAssociationMappings());
     }
 
     /**
