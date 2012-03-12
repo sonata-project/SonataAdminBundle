1208130fdeedb98b7181a5abfb90eadd4ccef38e
Andrey Suslin <andrey.suslin@intechsystems.lv>
2012-03-12 11:52:40 +0200

    Added new configuration parameter in "templates" for "list__batch.html.twig" tempalte.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -138,6 +138,7 @@ class Configuration implements ConfigurationInterface
                         ->scalarNode('list_block')->defaultValue('SonataAdminBundle:Block:block_admin_list.html.twig')->cannotBeEmpty()->end()
                         ->scalarNode('short_object_description')->defaultValue('SonataAdminBundle:Helper:short-object-description.html.twig')->cannotBeEmpty()->end()
                         ->scalarNode('delete')->defaultValue('SonataAdminBundle:CRUD:delete.html.twig')->cannotBeEmpty()->end()
+                        ->scalarNode('batch')->defaultValue('SonataAdminBundle:CRUD:list__batch.html.twig')->cannotBeEmpty()->end()
                     ->end()
                 ->end()
 
