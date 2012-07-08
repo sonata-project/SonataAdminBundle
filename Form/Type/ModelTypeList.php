426f23ce1586a1f85f0bb4f21e84bf4a0704390b
Thomas Rabaix <thomas.rabaix@sonata-project.org>
2012-07-07 20:12:11 -0400

    Add ModelTypeList

---
+++ Form/Type/ModelTypeList.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace Sonata\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormViewInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Sonata\AdminBundle\Form\DataTransformer\ModelToIdTransformer;
+
+/**
+ * This type is used to render an hidden input text and 3 links
+ *   - an add form modal
+ *   - a list modal to select the targetted entities
+ *   - a clear selection link
+ */
+class ModelTypeList extends AbstractType
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->resetViewTransformers()
+            ->addViewTransformer(new ModelToIdTransformer($options['model_manager'], $options['class']));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormViewInterface $view, FormInterface $form, array $options)
+    {
+        if ($view->hasVar('sonata_admin')) {
+            $parameters = $view->getVar('sonata_admin');
+
+            // set the correct edit mode
+            $parameters['edit'] = 'list';
+
+            $view->setVar('sonata_admin', $parameters);
+        }
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'model_manager'     => null,
+            'class'             => null,
+            'parent'            => 'text',
+        ));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getParent()
+    {
+        return 'text';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'sonata_type_model_list';
+    }
+}
--- UPGRATE-2.1.md
+++ UPGRATE-2.1.md
@@ -1,6 +1,8 @@
 UPGRADE FROM 2.0 to 2.1
 =======================
 
+** Work In Progress - Please do not use this code in production **
+
 ### Form
 
   * Due to some refactoring in the Form Component, some types definition have been changed
@@ -9,5 +11,6 @@ UPGRADE FROM 2.0 to 2.1
       * sonata_type_model : this type now only render a standard select widget or a list
         widget (if multiple option is set)
 
-
+      * sonata_type_model_list : this type replaces the option ``edit = list`` provided as
+        a 4th arguments on the ``sonata_type_model``
 
