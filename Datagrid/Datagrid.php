fa9d073dac22a5c21530c37f8059d280f0711715
WouterJ <waldio.webdesign@gmail.com>
2013-01-28 22:35:06 +0100

    Fixed deprecation call

--- Datagrid/Datagrid.php
+++ Datagrid/Datagrid.php
@@ -100,7 +100,7 @@ class Datagrid implements DatagridInterface
         }
 
         $this->formBuilder->add('_sort_by', 'hidden');
-        $this->formBuilder->get('_sort_by')->appendClientTransformer(new CallbackTransformer(
+        $this->formBuilder->get('_sort_by')->addViewTransformer(new CallbackTransformer(
             function($value) { return $value; },
             function($value) { return $value instanceof FieldDescriptionInterface ? $value->getName() : $value; }
         ));
