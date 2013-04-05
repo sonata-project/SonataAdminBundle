0eb4157fc96edc719901d78864e126d297784640
Viliam Husár <viliam.husar@imatic.cz>
2013-04-05 15:31:15 +0200

    Removed second argument.

--- Admin/Admin.php
+++ Admin/Admin.php
@@ -1993,11 +1993,6 @@ abstract class Admin implements AdminInterface, DomainObjectInterface
                     $this->trans($this->getLabelTranslatorStrategy()->getLabel(sprintf('%s_%s', $this->getClassnameLabel(), $action), 'breadcrumb', 'link'))
                 );
             }
-        } elseif ($action != 'list' && $this->hasSubject()) {
-            $breadcrumbs = $child->getBreadcrumbsArray(
-                $this->trans($this->getLabelTranslatorStrategy()->getLabel(sprintf('%s_%s', $this->getClassnameLabel(), $action), 'breadcrumb', 'link')),
-                $this->toString($this->getSubject())
-            );
         } elseif ($action != 'list') {
             $breadcrumbs = $child->getBreadcrumbsArray(
                 $this->trans($this->getLabelTranslatorStrategy()->getLabel(sprintf('%s_%s', $this->getClassnameLabel(), $action), 'breadcrumb', 'link'))
