58d991cffbd89a1fc42d6392e77854dd2ebb472d
luxemate <thunderbird@inbox.lv>
2013-04-06 11:46:44 +0300

    FIX: Bug with __toString call and the BC

--- Twig/Extension/SonataAdminExtension.php
+++ Twig/Extension/SonataAdminExtension.php
@@ -213,7 +213,19 @@ class SonataAdminExtension extends \Twig_Extension
         }
 
         if (null === $propertyPath) {
-            return call_user_func(array($element, $propertyPath));
+            // For BC kept associated_tostring option behavior
+            $method = $fieldDescription->getOption('associated_tostring', '__toString');
+
+            if (!method_exists($element, $method)) {
+                throw new \RuntimeException(sprintf(
+                    'You must define an `associated_property` option or create a `%s::__toString` method to the field option %s from service %s is ',
+                    get_class($element),
+                    $fieldDescription->getName(),
+                    $fieldDescription->getAdmin()->getCode()
+                ));
+            }
+
+            return call_user_func(array($element, $method));
         }
 
         return PropertyAccess::getPropertyAccessor()->getValue($element, $propertyPath);
