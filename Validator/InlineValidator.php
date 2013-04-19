bd08b55ed62e88d4b18ce516ebb5281ef82a2194
Thomas Ploch <profiploch@gmail.com>
2013-04-19 09:07:10 +0200

    Implemented ConstraintValidator::validate()
    Since version 2.1 of symfony/validator, ConstraintValidator::isValid()
    is deprecated an triggers an E_DEPRECATED error.
    
    Unfortunately the error handling in some circumstances triggers
    SIGSEGV in PHP-FPM (I havn't been able to pinpoint the exact cause).
    
    Now isValid() is just a BC wrapper for validate() not triggering any
    errors.

--- Validator/InlineValidator.php
+++ Validator/InlineValidator.php
@@ -33,7 +33,7 @@ class InlineValidator extends ConstraintValidator
     /**
      * {@inheritDoc}
      */
-    public function isValid($value, Constraint $constraint)
+    public function validate($value, Constraint $constraint)
     {
         $errorElement = new ErrorElement(
             $value,
@@ -55,7 +55,5 @@ class InlineValidator extends ConstraintValidator
         }
 
         call_user_func($function, $errorElement, $value);
-
-        return true;
     }
 }
