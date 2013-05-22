d17d17725da39df3bb5666b1a78942258e890c3f
Sjoerd Peters <speters@netvlies.net>
2013-05-22 17:23:40 +0200

    added feature to wire extensions dynamically using configuration

--- DependencyInjection/Compiler/ExtensionCompilerPass.php
+++ DependencyInjection/Compiler/ExtensionCompilerPass.php
@@ -40,5 +40,113 @@ class ExtensionCompilerPass implements CompilerPassInterface
             $container->getDefinition($target)
                 ->addMethodCall('addExtension', array(new Reference($id)));
         }
+
+        $extensionConfig = $container->getParameter('sonata.admin.extension.map');
+        $extensionMap = $this->flattenExtensionConfiguration($extensionConfig);
+
+        foreach ($container->findTaggedServiceIds('sonata.admin') as $id => $attributes) {
+            $admin = $container->getDefinition($id);
+            $extensions = $this->getExtensionsForAdmin($id, $admin, $container, $extensionMap);
+
+            foreach ($extensions as $extension) {
+                if(!$container->hasDefinition($extension)){
+                    throw new \InvalidArgumentException(sprintf('Unable to find extension service for id %s', $extension));
+                }
+                $admin->addMethodCall('addExtension', array(new Reference($extension)));
+            }
+        }
+    }
+
+    /**
+     * @param string $id
+     * @param Definition $admin
+     * @param ContainerBuilder $container
+     * @param array $extensionMap
+     * @return array
+     */
+    protected function getExtensionsForAdmin($id, Definition $admin, ContainerBuilder $container, array $extensionMap)
+    {
+        $extensions = array();
+        $class = $classReflection = $subjectReflection = null;
+
+        $excludes = $extensionMap['excludes'];
+        unset($extensionMap['excludes']);
+
+        foreach ($extensionMap as $type => $subjects) {
+            foreach ($subjects as $subject => $extensionList) {
+
+                if('admins' == $type){
+                    if($id == $subject){
+                        $extensions = array_merge($extensions, $extensionList);
+                    }
+                } else {
+                    $class = $this->getManagedClass($admin, $container);
+                    $classReflection = new \ReflectionClass($class);
+                    $subjectReflection = new \ReflectionClass($subject);
+                }
+
+                if('instanceof' == $type){
+                    if($subjectReflection->getName() == $classReflection->getName() || $classReflection->isSubclassOf($subject)){
+                        $extensions = array_merge($extensions, $extensionList);
+                    }
+                }
+
+                if('implements' == $type){
+                    if($classReflection->implementsInterface($subject)){
+                        $extensions = array_merge($extensions, $extensionList);
+                    }
+                }
+
+                if('extends' == $type){
+                    if($classReflection->isSubclassOf($subject)){
+                        $extensions = array_merge($extensions, $extensionList);
+                    }
+                }
+            }
+        }
+
+        if(isset($excludes[$id])){
+            $extensions = array_diff($extensions, $excludes);
+        }
+        return $extensions;
+    }
+
+    /**
+     * Resolves the class argument of the admin to an actual class (in case of %parameter%)
+     *
+     * @param Definition $admin
+     * @param ContainerBuilder $container
+     * @return string
+     */
+    protected function getManagedClass(Definition $admin, ContainerBuilder $container)
+    {
+        return $container->getParameterBag()->resolveValue($admin->getArgument(1));
+    }
+
+    /**
+     * @param array $config
+     * @return array
+     */
+    protected function flattenExtensionConfiguration(array $config)
+    {
+        $extensionMap = array(
+            'excludes'      => array(),
+            'admins'        => array(),
+            'implements'    => array(),
+            'extends'       => array(),
+            'instanceof'    => array(),
+        );
+
+        foreach ($config as $extension => $options) {
+            foreach ($options as $key => $value) {
+                foreach ($value as $source) {
+                    if(!isset($extensionMap[$key][$source])){
+                        $extensionMap[$key][$source] = array();
+                    }
+                    array_push($extensionMap[$key][$source], $extension);
+                }
+            }
+        }
+        return $extensionMap;
     }
 }
