2efdc9d4c49722d6ef708170126bfc8c6935de5f
Brice LALU <brice.lalu@free.fr>
2013-06-19 11:26:03 +0200

    request: Add $parameter option given to 'call_user_func' -fix indentation -parameter renamed in parameters -call_user_func_array is used instead of call_user_func -default value for parameters is defined (array()) -some tests and documentation added

--- Admin/BaseFieldDescription.php
+++ Admin/BaseFieldDescription.php
@@ -315,21 +315,22 @@ abstract class BaseFieldDescription implements FieldDescriptionInterface
         $camelizedFieldName = self::camelize($fieldName);
 
         $getters = array();
-
+        $parameters = array();
+        
         // prefer method name given in the code option
         if ($this->getOption('code')) {
             $getters[] = $this->getOption('code');
         }
-        
-        if($this->getOption('parameter')){
-            $parameter = $this->getOption('parameter');
+        // parameters for the method given in the code option
+        if($this->getOption('parameters')){
+            $parameters = $this->getOption('parameters');
         }
-            $getters[] = 'get' . $camelizedFieldName;
-            $getters[] = 'is' . $camelizedFieldName;
+        $getters[] = 'get' . $camelizedFieldName;
+        $getters[] = 'is' . $camelizedFieldName;
         
         foreach ($getters as $getter) {
             if (method_exists($object, $getter)) {
-                return call_user_func(array($object, $getter),$parameter);
+                return call_user_func_array(array($object, $getter),$parameters);
             }
         }
 
