9025363c02f1f392b59a73ba1426b119faa60390
Thomas Rabaix <thomas.rabaix@ekino.com>
2013-08-12 17:41:44 +0200

    Fix camelize

--- Admin/BaseFieldDescription.php
+++ Admin/BaseFieldDescription.php
@@ -403,7 +403,9 @@ abstract class BaseFieldDescription implements FieldDescriptionInterface
      */
     public static function camelize($property)
     {
-        return Container::camelize($property);
+        return preg_replace_callback('/(^|[_. ])+(.)/', function ($match) {
+            return ('.' === $match[1] ? '_' : '') . strtoupper($match[2]);
+        }, $property);
     }
 
     /**
