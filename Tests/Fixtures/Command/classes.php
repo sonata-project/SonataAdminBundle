aef6d2ba899964b278a48f89c110fd915982581b
Andrej Hudec <pulzarraider2@gmail.com>
2013-09-02 20:44:05 +0200

    Added CreateClassCacheCommandTest

---
+++ Tests/Fixtures/Command/classes.php
@@ -0,0 +1,29 @@
+<?php 
+namespace Sonata\AdminBundle\Tests\Fixtures\Controller
+{
+abstract class AbstractFooAdminController
+{
+public function bazAction()
+{
+}
+}
+}
+namespace Sonata\AdminBundle\Tests\Fixtures\Controller
+{
+use Sonata\AdminBundle\Tests\Fixtures\Controller\AbstractFooAdminController;
+class FooAdminController extends AbstractFooAdminController
+{
+public function fooAction($baz)
+{
+}
+}
+}
+namespace Sonata\AdminBundle\Tests\Fixtures\Controller
+{
+class BarAdminController
+{
+public function barAction()
+{
+}
+}
+}
---
+++ Tests/Fixtures/Controller/AbstractFooAdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sonata\AdminBundle\Tests\Fixtures\Controller;
+
+/**
+ * Some documentation about abstract controller
+ */
+abstract class AbstractFooAdminController
+{
+    /**
+     * Bar action
+     */
+    public function bazAction()
+    {
+    }
+}
