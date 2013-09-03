9cf05e521507cc6ea24134707ee46ae4c5a61d78
Benjamin Scholler <benjamin.scholler@sheknows.com>
2013-09-03 15:32:49 -0700

    Supporting toArray method when cursor is found during findUsers retrieval in aclAction.

--- Controller/CRUDController.php
+++ Controller/CRUDController.php
@@ -770,7 +770,12 @@ class CRUDController extends Controller
 
         $this->admin->setSubject($object);
         $aclUsers = $this->getAclUsers();
-        
+        if (is_object($aclUsers)) {
+             if (method_exists($aclUsers, 'toArray')) {
+                 $aclUsers = $aclUsers->toArray();
+             }
+        }
+
         $adminObjectAclManipulator = $this->get('sonata.admin.object.manipulator.acl.admin');
         $adminObjectAclData = new AdminObjectAclData(
             $this->admin,
