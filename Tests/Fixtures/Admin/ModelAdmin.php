df69d94850ccd093df7f34016b90855d7c92e62a
Andrej Hudec <pulzarraider2@gmail.com>
2013-09-08 17:43:37 +0200

    Change ambiguous inline action "view" to "show".
    Deleted list__action_view.html.twig template, added deprecated info in ListMapper.

--- Tests/Fixtures/Admin/ModelAdmin.php
+++ Tests/Fixtures/Admin/ModelAdmin.php
@@ -33,7 +33,7 @@ class ModelAdmin extends Admin
             ->add('baz')
             ->add('_action', 'actions', array(
                 'actions' => array(
-                    'view' => array(),
+                    'show' => array(),
                     'edit' => array(),
                     'delete' => array(),
                 )
