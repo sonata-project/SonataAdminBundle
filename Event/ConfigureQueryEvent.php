8f0e8bd1831d718f93bfaf0104a03911d7c5f4ef
Andrej Hudec <pulzarraider2@gmail.com>
2013-11-02 13:17:16 +0100

    Added tests for ConfigureEvent, ConfigureQueryEvent and PersistenceEvent

--- Event/ConfigureQueryEvent.php
+++ Event/ConfigureQueryEvent.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Sonata package.
  *
- * (c) 2010-2011 Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
