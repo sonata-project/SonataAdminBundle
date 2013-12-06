db85522c3c8aa3c6753d1ccf418e1e0dff0ea2d9
Thomas Rabaix <thomas.rabaix@sonata-project.org>
2013-12-06 10:46:26 +0100

    Force setlocale to fix issue with some tests

--- Tests/tests/bootstrap.php
+++ Tests/tests/bootstrap.php
@@ -9,6 +9,9 @@
  * file that was distributed with this source code.
  */
 
+// fix encoding issue while running text on different host with different locale configuration
+setlocale(LC_ALL, "en_US.UTF-8");
+
 if (file_exists($file = __DIR__.'/autoload.php')) {
     require_once $file;
 } elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
