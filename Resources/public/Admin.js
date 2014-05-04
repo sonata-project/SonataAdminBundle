3fec0da107e2c4754ee69829f0b08669e0c9484c
Toni Rudolf <toni.rudolf@gmail.com>
2014-05-04 11:39:09 +0200

    don't use iCheck in bootstrap button group

--- Resources/public/Admin.js
+++ Resources/public/Admin.js
@@ -103,7 +103,7 @@ var Admin = {
     },
     setup_icheck: function(subject) {
         if (window.SONATA_CONFIG && window.SONATA_CONFIG.USE_ICHECK) {
-            jQuery("input[type='checkbox'], input[type='radio']", subject).iCheck({
+            jQuery("input[type='checkbox']:not('label.btn>input'), input[type='radio']:not('label.btn>input')", subject).iCheck({
                 checkboxClass: 'icheckbox_minimal',
                 radioClass: 'iradio_minimal'
             });
