20b4c3b68e42f165a441b42d3e5c35e598cc73e6
Thomas Rabaix <thomas.rabaix@gmail.com>
2014-10-16 15:41:28 +0200

    cascade form name for embedded form

--- Tests/Form/Extension/Field/Type/FormTypeFieldExtensionTest.php
+++ Tests/Form/Extension/Field/Type/FormTypeFieldExtensionTest.php
@@ -50,7 +50,12 @@ class FormTypeFieldExtensionTest extends \PHPUnit_Framework_TestCase
     {
         $eventDispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
 
+        $parentFormView = new FormView();
+        $parentFormView->vars['sonata_admin_enabled'] = false;
+
         $formView = new FormView();
+        $formView->parent = $parentFormView;
+
         $options = array();
         $config = new FormConfigBuilder('test', 'stdClass', $eventDispatcher, $options);
         $form = new Form($config);
@@ -80,7 +85,7 @@ class FormTypeFieldExtensionTest extends \PHPUnit_Framework_TestCase
     public function testbuildViewWithWithSonataAdmin()
     {
         $admin = $this->getMock('Sonata\AdminBundle\Admin\AdminInterface');
-        $admin->expects($this->once())->method('getCode')->will($this->returnValue('my.admin.reference'));
+        $admin->expects($this->exactly(2))->method('getCode')->will($this->returnValue('my.admin.reference'));
 
         $eventDispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
 
@@ -94,6 +99,9 @@ class FormTypeFieldExtensionTest extends \PHPUnit_Framework_TestCase
         $config->setAttribute('sonata_admin_enabled', true);
 
         $form = new Form($config);
+
+        $formView->parent = new FormView();
+        $formView->parent->vars['sonata_admin_enabled'] = false;
 
         $formView->vars['block_prefixes'] = array('form', 'field', 'text', '_s50b26aa76cb96_username');
 
@@ -118,5 +126,47 @@ class FormTypeFieldExtensionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $formView->vars['block_prefixes']);
         $this->assertTrue($formView->vars['sonata_admin_enabled']);
         $this->assertEquals('help text', $formView->vars['sonata_help']);
+    }
+
+    public function testbuildViewWithNestedForm()
+    {
+        $eventDispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+
+        $formView = new FormView();
+        $formView->vars['name'] = 'format';
+
+        $options = array();
+        $config = new FormConfigBuilder('test', 'stdClass', $eventDispatcher, $options);
+        $config->setAttribute('sonata_admin', array('admin' => false));
+
+        $form = new Form($config);
+
+        $formView->parent = new FormView();
+        $formView->parent->vars['sonata_admin_enabled'] = true;
+        $formView->parent->vars['sonata_admin_code'] = 'parent_code';
+        $formView->parent->vars['name'] = 'settings';
+
+        $formView->vars['block_prefixes'] = array('form', 'field', 'text', '_s50b26aa76cb96_settings_format');
+
+        $extension = new FormTypeFieldExtension();
+        $extension->buildView($formView, $form, array(
+            'sonata_help' => 'help text'
+        ));
+
+        $this->assertArrayHasKey('block_prefixes', $formView->vars);
+        $this->assertArrayHasKey('sonata_admin_enabled', $formView->vars);
+        $this->assertArrayHasKey('sonata_admin', $formView->vars);
+
+        $expected = array(
+            'form',
+            'field',
+            'text',
+            'parent_code_text',
+            'parent_code_text_settings_format',
+            'parent_code_text_settings_settings_format',
+        );
+
+        $this->assertEquals($expected, $formView->vars['block_prefixes']);
+        $this->assertTrue($formView->vars['sonata_admin_enabled']);
     }
 }
