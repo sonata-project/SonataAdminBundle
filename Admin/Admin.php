4bb166df71ac7825c36854bf9a2d84d3526480c3
Patrick Li <patrick@satellitemedia.co.nz>
2015-01-28 09:18:15 +1300

    Allow UUID to be used as id parameter

--- Admin/Admin.php
+++ Admin/Admin.php
@@ -1743,7 +1743,7 @@ abstract class Admin implements AdminInterface, DomainObjectInterface
     {
         if ($this->subject === null && $this->request) {
             $id = $this->request->get($this->getIdParameter());
-            if (!preg_match('#^[0-9A-Fa-f]+$#', $id)) {
+            if (!preg_match('#^[-0-9A-Fa-f]+$#', $id)) {
                 $this->subject = false;
             } else {
                 $this->subject = $this->getModelManager()->find($this->class, $id);
