80aa1ce09c4ab18e23eb37e879f0d31aa40c0fcb
Emmanuel Vella <vella.emmanuel@gmail.com>
2015-03-06 15:57:57 +0100

    Add advanced filters toggle link

--- Resources/public/App.js
+++ Resources/public/App.js
@@ -93,6 +93,15 @@ $(function() {
         box.slideUp();
     });
 
+    /* Advanced filters */
+    if ($('.advanced-filter :input:visible').filter(function () { return $(this).val() }).length === 0) {
+        $('.advanced-filter').hide();
+    };
+
+    $('[data-toggle="advanced-filter"]').click(function() {
+        $('.advanced-filter').toggle();
+    });
+
     /* Sidebar tree view */
     $(".sidebar .treeview").tree();
 
