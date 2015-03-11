73b6bc8d0bcec9d7fabdf076521e76ba0384e854
NoScopie <kevin.tsyen@gmail.com>
2015-03-11 09:07:39 +0100

    Update AdminHelper.php
    Fix to support PHP 5.5

--- Admin/AdminHelper.php
+++ Admin/AdminHelper.php
@@ -185,6 +185,8 @@ class AdminHelper
      */
     public function camelize($property)
     {
-        return preg_replace(array('/(^|_| )+(.)/e', '/\.(.)/e'), array("strtoupper('\\2')", "'_'.strtoupper('\\1')"), $property);
+        return preg_replace_callback('/(^|[_. ])+(.)/', function ($match) {
+            return ('.' === $match[1] ? '_' : '') . strtoupper($match[2]);
+        }, $property);
     }
 }
