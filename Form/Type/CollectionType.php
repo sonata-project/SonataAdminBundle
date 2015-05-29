eedafc91694b223b62d8f3b1778d62968845b453
Jose Lopes <josluis.lopes@gmail.com>
2015-05-29 15:11:34 +0100

    Normalizing sonata_type_native_collection - styles and options Removed the btn list since we dont really do anything with it Add the missing classes that actually do the action

--- Form/Type/CollectionType.php
+++ Form/Type/CollectionType.php
@@ -13,6 +13,9 @@
 namespace Sonata\AdminBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * This type wrap native `collection` form type and render `add` and `delete`
@@ -28,6 +31,30 @@ class CollectionType extends AbstractType
     public function getParent()
     {
         return 'collection';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['btn_add'] = $options['btn_add'];
+        $view->vars['btn_delete'] = $options['btn_delete'];
+        $view->vars['btn_catalogue'] = $options['btn_catalogue'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+        $resolver->setDefaults(array(
+            'auto_initialize' => false,
+            'btn_add'         => 'link_add',
+            'btn_delete'      => 'link_delete',
+            'btn_catalogue'   => 'SonataAdminBundle'
+        ));
     }
 
     /**
