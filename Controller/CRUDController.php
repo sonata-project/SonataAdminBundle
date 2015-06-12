5cddf62d05a55b4cfa716d2a45d6ace683a47f14
Thomas Rabaix <thomas.rabaix@gmail.com>
2015-06-12 12:09:12 +0200

    backport 2.3 patch so fix XSS issue

--- Controller/CRUDController.php
+++ Controller/CRUDController.php
@@ -244,7 +244,7 @@ class CRUDController extends Controller
 
                 $this->addFlash(
                     'sonata_flash_success',
-                    $this->admin->trans('flash_delete_success', array('%name%' => $this->admin->toString($object)),
+                    $this->admin->trans('flash_delete_success', array('%name%' => $this->escapeHtml($this->admin->toString($object))),
                     'SonataAdminBundle')
                 );
 
@@ -256,7 +256,7 @@ class CRUDController extends Controller
 
                 $this->addFlash(
                     'sonata_flash_error',
-                    $this->admin->trans('flash_delete_error', array('%name%' => $this->admin->toString($object)),
+                    $this->admin->trans('flash_delete_error', array('%name%' => $this->escapeHtml($this->admin->toString($object))),
                     'SonataAdminBundle')
                 );
             }
@@ -320,7 +320,7 @@ class CRUDController extends Controller
                     ));
                 }
 
-                $this->addFlash('sonata_flash_success', $this->admin->trans('flash_edit_success', array('%name%' => $this->admin->toString($object)), 'SonataAdminBundle'));
+                $this->addFlash('sonata_flash_success', $this->admin->trans('flash_edit_success', array('%name%' => $this->escapeHtml($this->admin->toString($object)), 'SonataAdminBundle')));
 
                 // redirect to edit mode
                 return $this->redirectTo($object);
@@ -329,7 +329,7 @@ class CRUDController extends Controller
             // show an error message if the form failed validation
             if (!$isFormValid) {
                 if (!$this->isXmlHttpRequest()) {
-                    $this->addFlash('sonata_flash_error', $this->admin->trans('flash_edit_error', array('%name%' => $this->admin->toString($object)), 'SonataAdminBundle'));
+                    $this->addFlash('sonata_flash_error', $this->admin->trans('flash_edit_error', array('%name%' => $this->escapeHtml($this->admin->toString($object)), 'SonataAdminBundle')));
                 }
             } elseif ($this->isPreviewRequested()) {
                 // enable the preview template if the form was valid and preview was requested
@@ -522,7 +522,7 @@ class CRUDController extends Controller
                     ));
                 }
 
-                $this->addFlash('sonata_flash_success', $this->admin->trans('flash_create_success', array('%name%' => $this->admin->toString($object)), 'SonataAdminBundle'));
+                $this->addFlash('sonata_flash_success', $this->admin->trans('flash_create_success', array('%name%' => $this->escapeHtml($this->admin->toString($object)), 'SonataAdminBundle')));
 
                 // redirect to edit mode
                 return $this->redirectTo($object);
@@ -531,7 +531,7 @@ class CRUDController extends Controller
             // show an error message if the form failed validation
             if (!$isFormValid) {
                 if (!$this->isXmlHttpRequest()) {
-                    $this->addFlash('sonata_flash_error', $this->admin->trans('flash_create_error', array('%name%' => $this->admin->toString($object)), 'SonataAdminBundle'));
+                    $this->addFlash('sonata_flash_error', $this->admin->trans('flash_create_error', array('%name%' => $this->escapeHtml($this->admin->toString($object)), 'SonataAdminBundle')));
                 }
             } elseif ($this->isPreviewRequested()) {
                 // pick the preview template if the form was valid and preview was requested
@@ -842,6 +842,18 @@ class CRUDController extends Controller
         $this->get('session')
              ->getFlashBag()
              ->add($type, $message);
+    }
+
+    /**
+     * Escape string for html output
+     *
+     * @param string $s
+     *
+     * @return string
+     */
+    protected function escapeHtml($s)
+    {
+        return htmlspecialchars($s, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
     }
 
     /**
