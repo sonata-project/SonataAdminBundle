c9d31ef5f5c6214e950b4fa51dd08ab8b12471b3
Oskar Stark <oskarstark@googlemail.com>
2015-07-07 12:48:10 +0200

    bump version year

--- Resources/doc/conf.py
+++ Resources/doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Sonata ~ AdminBundle'
-copyright = u'2010-2014, Thomas Rabaix'
+copyright = u'2010-2015, Thomas Rabaix'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
