9247d2474e926a4767847ca1c3e2c63d66811ee1
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-05-16 14:33:08 +0200

    Restore deprecated class

---
+++ Admin/AdminExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\AdminBundle\Admin;
+
+@trigger_error(
+    'The '.__NAMESPACE__.'\AdminExtension class is deprecated since version 3.x and will be removed in 4.0.'
+    .' Use '.__NAMESPACE__.'\AbstractAdminExtension instead.',
+    E_USER_DEPRECATED
+);
+
+/**
+ * @deprecated since version 3.x, to be removed in 4.0. Use Sonata\AdminBundle\AbstractAdminExtension instead.
+ *
+ * @author Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ */
+abstract class AdminExtension extends AbstractAdminExtension
+{
+}
