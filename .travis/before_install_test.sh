3b75abf872ba18adbdeb2ef1d2db397b8a8c9bca
SonataCI <thomas+ci@sonata-project.org>
2016-05-27 03:00:16 +0200

    DevKit updates

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -4,6 +4,10 @@ set -ev
 if [ "${TRAVIS_PHP_VERSION}" != "hhvm" ]; then
     mv "$HOME/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini" /tmp
     echo "memory_limit=3072M" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+
+    if [ ${TRAVIS_PHP_VERSION} '<' '7.0' ]; then
+        echo "extension=mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+    fi
 fi
 
 # To be removed when following PR will be merged: https://github.com/travis-ci/travis-build/pull/718
