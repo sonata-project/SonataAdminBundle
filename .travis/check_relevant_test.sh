8e91a5b0614bcad94aa4dbcd065987716c73c98c
SonataCI <thomas+ci@sonata-project.org>
2016-06-02 11:28:50 +0200

    DevKit updates

--- .travis/check_relevant_test.sh
+++ .travis/check_relevant_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ev
 
-RELEVANT_FILES=$(git diff --name-only HEAD upstream/${TRAVIS_BRANCH} -- *.{php,yml,xml,twig,js,css,json})
+RELEVANT_FILES=$(git diff --name-only HEAD upstream/${TRAVIS_BRANCH} -- '*.php' '*.yml' '*.xml' '*.twig' '*.js' '*.css' '*.json')
 
 if [[ -z ${RELEVANT_FILES} ]]; then echo -n 'KO'; exit 0; fi;
