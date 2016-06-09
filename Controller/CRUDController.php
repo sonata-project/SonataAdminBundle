0bb52a19aac5bf7e6bdf81da34dabb166f28672d
core23 <mail@core23.de>
2016-06-09 18:31:17 +0200

    Removed unnecessary check for AdminInterface::preValidate

--- Controller/CRUDController.php
+++ Controller/CRUDController.php
@@ -256,10 +256,7 @@ class CRUDController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
-            //TODO: remove this check for 4.0
-            if (method_exists($this->admin, 'preValidate')) {
-                $this->admin->preValidate($object);
-            }
+            $this->admin->preValidate($object);
             $isFormValid = $form->isValid();
 
             // persist if the form was valid and if in preview mode the preview was approved
@@ -492,10 +489,7 @@ class CRUDController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
-            //TODO: remove this check for 4.0
-            if (method_exists($this->admin, 'preValidate')) {
-                $this->admin->preValidate($object);
-            }
+            $this->admin->preValidate($object);
             $isFormValid = $form->isValid();
 
             // persist if the form was valid and if in preview mode the preview was approved
