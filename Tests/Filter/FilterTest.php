dbb3ff89ad3dd72d1daea3070d321d492e548e60
François-Xavier de Guillebon <deguif@gmail.com>
2016-08-31 18:12:32 +0200

    Return classname when using the new form API (#4113)
    The new form API expects classnames instead of form type names,
    and a deprecation warning can be triggered when using
    a filter that does not override the default field type.

--- Tests/Filter/FilterTest.php
+++ Tests/Filter/FilterTest.php
@@ -19,7 +19,9 @@ class FilterTest extends \PHPUnit_Framework_TestCase
     {
         $filter = new FooFilter();
 
-        $this->assertSame('text', $filter->getFieldType());
+        $this->assertSame(method_exists('Symfony\Component\Form\AbstractType', 'getBlockPrefix')
+            ? 'Symfony\Component\Form\Extension\Core\Type\TextType'
+            : 'text', $filter->getFieldType());
         $this->assertSame(array('required' => false), $filter->getFieldOptions());
         $this->assertNull($filter->getLabel());
 
