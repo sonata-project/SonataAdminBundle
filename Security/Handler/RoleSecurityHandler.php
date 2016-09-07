34ca9e6863b84990cd198764bb7a3ce0f5326c2a
Baltox <marcanon45@laposte.net>
2016-09-07 16:04:16 +0200

    Check all role
    The role must be checked separately

--- Security/Handler/RoleSecurityHandler.php
+++ Security/Handler/RoleSecurityHandler.php
@@ -62,11 +62,12 @@ class RoleSecurityHandler implements SecurityHandlerInterface
             $attributes[$pos] = sprintf($this->getBaseRole($admin), $attribute);
         }
 
-        $attributes[] = sprintf($this->getBaseRole($admin), 'ALL');
+        $checkAllRole = sprintf($this->getBaseRole($admin), 'ALL');
 
         try {
             return $this->authorizationChecker->isGranted($this->superAdminRoles)
-                || $this->authorizationChecker->isGranted($attributes, $object);
+                || $this->authorizationChecker->isGranted($attributes, $object)
+                || $this->authorizationChecker->isGranted($checkAllRole, $object);
         } catch (AuthenticationCredentialsNotFoundException $e) {
             return false;
         }
