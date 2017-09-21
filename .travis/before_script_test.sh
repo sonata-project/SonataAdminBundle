c50c490d5f6a6b1187da38512c88e5c6d4e4d8ef
SonataCI <thomas+ci@sonata-project.org>
2017-09-21 02:05:08 +0200

    DevKit updates

--- .travis/before_script_test.sh
+++ .travis/before_script_test.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 set -ev
 
-if [ "$TRAVIS_PHP_VERSION" != "hhvm" ] && [ "$TRAVIS_PHP_VERSION" '<' '5.4' ]; then
-    PHP_INI_DIR="$HOME/.phpenv/versions/$(phpenv version-name)/etc/conf.d/"
-    XDEBUG_INI_FILE="/tmp/xdebug.ini"
-    if [ -f  "$XDEBUG_INI_FILE" ]; then
-        mv "$XDEBUG_INI_FILE" "$PHP_INI_DIR"
-    fi
-fi
