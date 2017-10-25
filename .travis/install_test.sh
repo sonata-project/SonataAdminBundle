19fa108560242768f674d62ef2a7f1e4f9dca4cf
SonataCI <thomas+ci@sonata-project.org>
2017-10-25 02:05:37 +0200

    DevKit updates

--- .travis/install_test.sh
+++ .travis/install_test.sh
@@ -19,6 +19,8 @@ chmod u+x "${HOME}/bin/coveralls"
 # To be removed when these issues are resolved:
 # https://github.com/composer/composer/issues/5355
 # https://github.com/composer/composer/issues/5030
-composer update --prefer-dist --no-interaction --prefer-stable --quiet --ignore-platform-reqs
+if [ "${COMPOSER_FLAGS}" = '--prefer-lowest' ]; then
+    composer update --prefer-dist --no-interaction --prefer-stable --quiet --ignore-platform-reqs
+fi
 
 composer update --prefer-dist --no-interaction --prefer-stable ${COMPOSER_FLAGS}
