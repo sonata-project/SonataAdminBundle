e44ea53a9c23e669a18095bd835b90175ffd2971
SonataCI <thomas+ci@sonata-project.org>
2017-11-11 02:05:09 +0100

    DevKit updates

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
 set -ev
 
-if [ "${TRAVIS_PHP_VERSION}" != "hhvm" ]; then
-    PHP_INI_DIR="$HOME/.phpenv/versions/$(phpenv version-name)/etc/conf.d/"
-    TRAVIS_INI_FILE="$PHP_INI_DIR/travis.ini"
-    echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
+PHP_INI_DIR="$HOME/.phpenv/versions/$(phpenv version-name)/etc/conf.d/"
+TRAVIS_INI_FILE="$PHP_INI_DIR/travis.ini"
+echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
 
-    
-    fi
+
 
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
