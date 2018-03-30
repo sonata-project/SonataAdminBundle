5782acb292909110f16d514de2f8175122bcf572
Koen Verheyen <verheyen.koen@gmail.com>
2018-03-30 15:07:38 +0200

    Fix for hidden jQuery UI dialog close button (#5047)
    * Moved bootstrap dependency up to fix jQuery UI dialog close button issue (#4511)
    
    * Applied Flint CI patch

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -378,10 +378,10 @@ class Configuration implements ConfigurationInterface
 
                                 'bundles/sonatacore/vendor/moment/min/moment.min.js',
 
+                                'bundles/sonatacore/vendor/bootstrap/dist/js/bootstrap.min.js',
+
                                 'bundles/sonataadmin/vendor/jqueryui/ui/minified/jquery-ui.min.js',
                                 'bundles/sonataadmin/vendor/jqueryui/ui/minified/i18n/jquery-ui-i18n.min.js',
-
-                                'bundles/sonatacore/vendor/bootstrap/dist/js/bootstrap.min.js',
 
                                 'bundles/sonatacore/vendor/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
 
