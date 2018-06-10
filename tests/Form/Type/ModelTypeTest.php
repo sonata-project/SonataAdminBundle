96dacc3f7d8f84358fcc21ce3791dfb01a25484b
Grégoire Paris <postmaster@greg0ire.fr>
2018-06-10 11:15:17 +0200

    Apply new CS fixer rules

--- tests/Form/Type/ModelTypeTest.php
+++ tests/Form/Type/ModelTypeTest.php
@@ -47,8 +47,8 @@ class ModelTypeTest extends TypeTestCase
         $this->assertNull($options['class']);
         $this->assertNull($options['property']);
         $this->assertNull($options['query']);
-        $this->assertSame(0, count($options['choices']));
-        $this->assertSame(0, count($options['preferred_choices']));
+        $this->assertCount(0, $options['choices']);
+        $this->assertCount(0, $options['preferred_choices']);
         $this->assertSame('link_add', $options['btn_add']);
         $this->assertSame('link_list', $options['btn_list']);
         $this->assertSame('link_delete', $options['btn_delete']);
@@ -76,8 +76,8 @@ class ModelTypeTest extends TypeTestCase
         $this->assertNull($options['class']);
         $this->assertNull($options['property']);
         $this->assertNull($options['query']);
-        $this->assertSame(0, count($options['choices']));
-        $this->assertSame(0, count($options['preferred_choices']));
+        $this->assertCount(0, $options['choices']);
+        $this->assertCount(0, $options['preferred_choices']);
         $this->assertSame('link_add', $options['btn_add']);
         $this->assertSame('link_list', $options['btn_list']);
         $this->assertSame('link_delete', $options['btn_delete']);
