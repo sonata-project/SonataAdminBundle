a4c40f119236651095e5d0131f0e8bf6208f11bc
Oskar Stark <oskarstark@googlemail.com>
2018-06-26 14:00:12 +0200

    fix travis

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -9,6 +9,14 @@ echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
 
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
+# symfony/maker-bundle only works with PHP 7 and higher
+if [ "$TRAVIS_PHP_VERSION" '>=' '7.0' ]; then
+    # but only with Symfony 3.4 and higher
+    if [ "$SYMFONY" != "" && "$SYMFONY" '>=' '3.4' ]; then
+        composer require "symfony/maker-bundle:1" --no-update
+    fi
+fi
+
 if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
 if [ "$SONATA_CORE" != "" ]; then composer require "sonata-project/core-bundle:$SONATA_CORE" --no-update; fi;
 if [ "$SONATA_BLOCK" != "" ]; then composer require "sonata-project/block-bundle:$SONATA_BLOCK" --no-update; fi;
