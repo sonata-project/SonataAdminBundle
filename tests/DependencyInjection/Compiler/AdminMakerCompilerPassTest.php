6e392d8d8e3ddd72a7a89d4c08b0dc2fb173093f
Oskar Stark <oskarstark@googlemail.com>
2018-06-27 07:28:13 +0200

    fix flintci

--- tests/DependencyInjection/Compiler/AdminMakerCompilerPassTest.php
+++ tests/DependencyInjection/Compiler/AdminMakerCompilerPassTest.php
@@ -28,7 +28,7 @@ class AdminMakerCompilerPassTest extends TestCase
     {
         if (5 == PHP_MAJOR_VERSION && !class_exists('Symfony\Component\Console\CommandLoader\CommandLoaderInterface')) {
             $this->markTestSkipped('Test only available for PHP 7 and SF 3.4');
-        }        
+        }
 
         parent::setUp();
         $this->adminMaker = $this->prophesize(Definition::class);
