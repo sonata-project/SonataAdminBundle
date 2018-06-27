2805b7d6ce7dbb059e49015fd4eaa7d4f6f5e179
Oskar Stark <oskarstark@googlemail.com>
2018-06-27 07:21:39 +0200

    fix travis

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -10,10 +10,13 @@ echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
 # symfony/maker-bundle only works with PHP 7 and higher
-if [ "$TRAVIS_PHP_VERSION" '>=' '7.0' ]; then
+if [ "$TRAVIS_PHP_VERSION" '>' '7.0' ]; then
     # but only with Symfony 3.4 and higher
-    if [ "$SYMFONY" != "" && "$SYMFONY" '>=' '3.4' ]; then
-        composer require "symfony/maker-bundle:1" --no-update
+    
+    if [ "$SYMFONY" != "" ]; then 
+        if [ "$SYMFONY" '>=' '3.4' ] ; then
+            composer require "symfony/maker-bundle:1" --no-update
+        fi
     else
         composer require "symfony/maker-bundle:1" --no-update
     fi
