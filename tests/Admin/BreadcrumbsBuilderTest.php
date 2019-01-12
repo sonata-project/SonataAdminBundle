78b8d82df13d72d3f11b9de385a65dbf1cec6d87
Grégoire Paris <postmaster@greg0ire.fr>
2019-01-12 17:57:23 +0100

    Add missing assertions

--- tests/Admin/BreadcrumbsBuilderTest.php
+++ tests/Admin/BreadcrumbsBuilderTest.php
@@ -509,6 +509,8 @@ class BreadcrumbsBuilderTest extends TestCase
         if ('list' == $action) {
             $admin->isChild()->willReturn(true);
             $menu->setUri(false)->shouldBeCalled();
+        } else {
+            $menu->setUri()->shouldNotBeCalled();
         }
         $request = $this->prophesize(Request::class);
         $request->get('slug')->willReturn('my-object');
