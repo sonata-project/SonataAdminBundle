e8cdd0cce2e09ff53845eaeb1f23f92122b52e25
SonataCI <thomas+ci@sonata-project.org>
2019-01-15 02:05:08 +0100

    DevKit updates

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Sonata ~ AdminBundle'
-copyright = u'2010-2015, Thomas Rabaix'
+project = u''
+copyright = u'2010-2019, Thomas Rabaix'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
