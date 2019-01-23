463a99df1afd42c4da0f9e2e0e27a12900117918
Sonata CI <thomas+ci@sonata-project.org>
2019-01-23 04:15:04 +0100

    DevKit updates (#5449)

--- .travis/before_script_test.sh
+++ .travis/before_script_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 
-if [ -f before_script_test.local.sh ]
+if [ -f .travis/before_script_test.local.sh ]
 then
-    ./before_script_test.local.sh
+    ./.travis/before_script_test.local.sh
 fi
