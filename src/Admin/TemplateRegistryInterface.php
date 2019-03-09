2e27895f3703c712bec1cf5bc96ef68c4d1f49d4
Oskar Stark <oskarstark@googlemail.com>
2019-03-09 17:43:00 +0100

    removed phpdoc

--- src/Admin/TemplateRegistryInterface.php
+++ src/Admin/TemplateRegistryInterface.php
@@ -15,40 +15,25 @@ namespace Sonata\AdminBundle\Admin;
 
 interface TemplateRegistryInterface
 {
-    public function __construct(array $templates);
-
     /**
      * Sets a specific template.
-     *
-     * @param string $name
-     * @param string $template
      */
     public function setTemplate(string $name, string $template): void;
 
     /**
      * Get all templates.
-     *
-     * @return array
      */
     public function getTemplates(): array;
 
     /**
      * Returns template.
      *
-     * @param string $name
-     *
      * @throws \InvalidArgumentException
-     *
-     * @return string
      */
     public function getTemplate(string $name): string;
 
     /**
      * Return true if the template is set.
-     *
-     * @param string $name
-     *
-     * @return bool
      */
     public function hasTemplate(string $name): bool;
 }
