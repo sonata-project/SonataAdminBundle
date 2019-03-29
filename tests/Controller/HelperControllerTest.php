3967d974e4d3dd31c7ec6d034ba0e0d0c3bda77a
Fran Moreno <franmomu@gmail.com>
2019-03-29 19:00:58 +0100

    Fix failing tests

--- tests/Controller/HelperControllerTest.php
+++ tests/Controller/HelperControllerTest.php
@@ -199,6 +199,7 @@ class HelperControllerTest extends TestCase
         $fieldDescription = $this->prophesize(FieldDescriptionInterface::class);
         $pool = $this->prophesize(Pool::class);
         $template = $this->prophesize(Template::class);
+        $template->render(Argument::cetera())->willReturn('some value');
         $translator = $this->prophesize(TranslatorInterface::class);
         $propertyAccessor = new PropertyAccessor();
         $templateRegistry = $this->prophesize(TemplateRegistryInterface::class);
@@ -246,6 +247,7 @@ class HelperControllerTest extends TestCase
         $fieldDescription = $this->prophesize(FieldDescriptionInterface::class);
         $modelManager = $this->prophesize(ModelManagerInterface::class);
         $template = $this->prophesize(Template::class);
+        $template->render(Argument::cetera())->willReturn('some value');
         $translator = $this->prophesize(TranslatorInterface::class);
         $propertyAccessor = new PropertyAccessor();
         $templateRegistry = $this->prophesize(TemplateRegistryInterface::class);
