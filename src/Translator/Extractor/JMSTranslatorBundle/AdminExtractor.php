77b6a6223c1fda8044f37f42133e01124e418ee1
Javier Spagnoletti <phansys@gmail.com>
2019-06-03 22:16:31 -0300

    Use Symfony contracts

--- src/Translator/Extractor/JMSTranslatorBundle/AdminExtractor.php
+++ src/Translator/Extractor/JMSTranslatorBundle/AdminExtractor.php
@@ -23,7 +23,7 @@ use Sonata\AdminBundle\Admin\BreadcrumbsBuilderInterface;
 use Sonata\AdminBundle\Admin\Pool;
 use Sonata\AdminBundle\Security\Handler\SecurityHandlerInterface;
 use Sonata\AdminBundle\Translator\LabelTranslatorStrategyInterface;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @final since sonata-project/admin-bundle 3.52
