c892afef162b3adef67ded745e758ed2592b5f9d
Oleg Andreyev <oleg.andreyev@ecentria.com>
2019-10-16 22:37:17 +0300

    !squash reverted renderJson return type

--- src/Controller/CRUDController.php
+++ src/Controller/CRUDController.php
@@ -1101,7 +1101,7 @@ class CRUDController implements ContainerAwareInterface
      *
      * @return JsonResponse with json encoded data
      */
-    protected function renderJson($data, $status = 200, $headers = []): Response
+    protected function renderJson($data, $status = 200, $headers = [])
     {
         return new JsonResponse($data, $status, $headers);
     }
