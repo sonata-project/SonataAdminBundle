110d8c73b841aeaa142ecae2f9b52db035c1024c
pratik07101999 <42681860+pratik07101999@users.noreply.github.com>
2019-10-19 10:23:55 +0530

    Calculator.php

---
+++ src/calculator.php
@@ -0,0 +1,11 @@
+<center><h2>CALCULATOR</h2>
+<br>
+<form action="Code.php" method="POST" >
+	Enter the Num1 :<input type="text" name="n1"><br>
+	Enter the Num2 :<input type="text" name="n2"><br>
+	<br>
+	<input type="submit" name="t3" value="Add">
+	<input type="submit" name="t3" value="Subtract">
+	<input type="submit" name="t3" value="Multiply">
+	<input type="submit" name="t3" value="Divide">
+</form></center>
