c5fe2a8f66f6be3a1a09049991476505bbe0f92a
Fran Moreno <franmomu@gmail.com>
2019-12-18 22:05:19 +0100

    Add minimal functional test (#5728)
    * Add minimal functional test
    
    * Do not reference sonata.templating to remove deprecations
    
    * Fix return type from string to array

---
+++ tests/Functional/Controller/DashboardActionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\AdminBundle\Tests\Functional\Controller;
+
+use PHPUnit\Framework\TestCase;
+use Sonata\AdminBundle\Tests\Functional\AppKernel;
+use Symfony\Bundle\FrameworkBundle\Client;
+
+final class DashboardActionTest extends TestCase
+{
+    public function testDashboard(): void
+    {
+        $client = new Client(new AppKernel());
+        $client->request('GET', '/dashboard');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+    }
+}
