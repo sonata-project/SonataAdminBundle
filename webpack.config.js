225b5a9830c1cd21703c91716192785415a43cd1
Silas Joisten <silasjoisten@gmail.com>
2020-02-01 10:31:24 +0100

    removed webpack chunk splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 var Encore = require('@symfony/webpack-encore');
 var path = require('path');
 
+const PluginPriorities = require('@symfony/webpack-encore/lib/plugins/plugin-priorities.js');
+const webpack = require('webpack');
+
 Encore
     .setOutputPath('src/Resources/public/dist')
     .setPublicPath('/bundles/sonataadmin/dist')
@@ -8,16 +11,20 @@ Encore
     .addEntry('sonata_admin', './src/Resources/public/js/sonata_admin.js')
     .autoProvidejQuery()
     .enableSassLoader()
-    .enableSingleRuntimeChunk()
-    .splitEntryChunks()
     .enableSourceMaps(!Encore.isProduction())
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
+    .enableVersioning(false)
     .addAliases({
         jquery: path.resolve(__dirname, 'node_modules/jquery/dist/jquery')
     })
+
+    // @TODO: remove for performance enhancement and add enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
+    //.enableSingleRuntimeChunk()
     //.splitEntryChunks()
-    //.enableVersioning(Encore.isProduction())
+    // @TODO: remove for performance enhancement and enable splitEntryChunks()
+    .addPlugin(new webpack.optimize.LimitChunkCountPlugin({maxChunks: 1}), PluginPriorities.DefinePlugin)
 ;
 
 module.exports = Encore.getWebpackConfig();
