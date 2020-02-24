395ed3d6185b5be1284863b7fedc8eab1cb33ef7
Oskar Stark <oskarstark@googlemail.com>
2020-02-24 12:44:03 +0100

    foo

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -6,7 +6,7 @@ TRAVIS_INI_FILE="$PHP_INI_DIR/travis.ini"
 echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
 
 
-sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
+sed --in-place "s/\"dev-master\":/\"dev-${GITHUB_SHA}\":/" composer.json
 
 if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
 if [ "$SONATA_CORE" != "" ]; then composer require "sonata-project/core-bundle:$SONATA_CORE" --no-update; fi;
--- .travis/check_relevant_test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -ev
-
-RELEVANT_FILES=$(git diff --name-only HEAD upstream/${TRAVIS_BRANCH} -- '*.php' '*.yml' '*.xml' '*.twig' '*.js' '*.css' '*.json')
-
-if [[ -z ${RELEVANT_FILES} ]]; then echo -n 'KO'; exit 0; fi;
