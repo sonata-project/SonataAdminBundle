0dad689534a06a20ff7d1c2ec1069ddb8db6cab0
Oskar Stark <oskarstark@googlemail.com>
2020-02-24 13:09:22 +0100

    try

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -ev
 
-PHP_INI_DIR="$HOME/.phpenv/versions/$(phpenv version-name)/etc/conf.d/"
-TRAVIS_INI_FILE="$PHP_INI_DIR/travis.ini"
-echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
-
-
 sed --in-place "s/\"dev-master\":/\"dev-${GITHUB_SHA}\":/" composer.json
 
 if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
