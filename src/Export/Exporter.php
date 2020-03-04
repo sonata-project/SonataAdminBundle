eda93959455961840c146f4f21931f63ecfd34d9
Vincent Langlet <vincentlanglet@hotmail.fr>
2020-03-05 00:27:17 +0100

    Stop using the core bundle exporter

--- src/Export/Exporter.php
+++ src/Export/Exporter.php
@@ -13,7 +13,13 @@ declare(strict_types=1);
 
 namespace Sonata\AdminBundle\Export;
 
-use Sonata\CoreBundle\Exporter\Exporter as BaseExporter;
+use Sonata\Exporter\Handler;
+use Sonata\Exporter\Source\SourceIteratorInterface;
+use Sonata\Exporter\Writer\CsvWriter;
+use Sonata\Exporter\Writer\JsonWriter;
+use Sonata\Exporter\Writer\XlsWriter;
+use Sonata\Exporter\Writer\XmlWriter;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 @trigger_error(
     'The '.__NAMESPACE__.'\Exporter class is deprecated since version 3.14 and will be removed in 4.0.'.
@@ -26,6 +32,51 @@ use Sonata\CoreBundle\Exporter\Exporter as BaseExporter;
  *
  * @deprecated since sonata-project/admin-bundle 3.14, to be removed in 4.0.
  */
-class Exporter extends BaseExporter
+class Exporter
 {
+    /**
+     * @param string $format
+     * @param string $filename
+     *
+     * @throws \RuntimeException
+     *
+     * @return StreamedResponse
+     */
+    public function getResponse($format, $filename, SourceIteratorInterface $source)
+    {
+        switch ($format) {
+            case 'xls':
+                $writer = new XlsWriter('php://output');
+                $contentType = 'application/vnd.ms-excel';
+
+                break;
+            case 'xml':
+                $writer = new XmlWriter('php://output');
+                $contentType = 'text/xml';
+
+                break;
+            case 'json':
+                $writer = new JsonWriter('php://output');
+                $contentType = 'application/json';
+
+                break;
+            case 'csv':
+                $writer = new CsvWriter('php://output', ',', '"', '\\', true, true);
+                $contentType = 'text/csv';
+
+                break;
+            default:
+                throw new \RuntimeException('Invalid format');
+        }
+
+        $callback = static function () use ($source, $writer) {
+            $handler = Handler::create($source, $writer);
+            $handler->export();
+        };
+
+        return new StreamedResponse($callback, 200, [
+            'Content-Type' => $contentType,
+            'Content-Disposition' => sprintf('attachment; filename="%s"', $filename),
+        ]);
+    }
 }
