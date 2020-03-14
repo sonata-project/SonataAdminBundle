6ed1c8f30679c53fcd3d5f0d31bcf85330077550
Fran Moreno <franmomu@gmail.com>
2020-03-14 13:10:36 +0100

    Proof dev-kit 503

--- .travis/install_test.sh
+++ .travis/install_test.sh
@@ -3,9 +3,6 @@ set -ev
 
 mkdir --parents "${HOME}/bin"
 
-wget "https://phar.phpunit.de/phpunit-${PHPUNIT_VERSION}.phar" --output-document="${HOME}/bin/phpunit"
-chmod u+x "${HOME}/bin/phpunit"
-
 # Coveralls client install
 wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
 chmod u+x "${HOME}/bin/coveralls"
