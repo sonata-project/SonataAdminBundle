e5e1cc12e01217448d5e7e4455665c3552380628
xavier Lembo <xlembo@eliberty.fr>
2020-04-10 15:33:22 +0200

    fix: add datetime in editable date form types for SetObjectFieldValueAction
    When using editable datetime formtype in datagrid configuration
    the case was not handled correctly since Action introduction.
    Until now only editable date fields were correctly handled.
    
    This PR fix this behaviour and add the datetime formtype as
    transformable to DateTime
    
    Tests have been added to illustrate the case, without the PR
    testSetObjectFieldValueActionWithDateTime wouldn't pass.

---
+++ tests/Action/Bafoo.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\AdminBundle\Tests\Action;
+
+/**
+ * Class Bafoo.
+ */
+class Bafoo
+{
+    private $dateProp;
+
+    private $datetimeProp;
+
+    /**
+     * @return mixed
+     */
+    public function getDateProp(): ?\DateTime
+    {
+        return $this->dateProp;
+    }
+
+    /**
+     * @param mixed $dateProp
+     */
+    public function setDateProp(\DateTime $dateProp): self
+    {
+        $this->dateProp = $dateProp;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDatetimeProp(): \DateTime
+    {
+        return $this->datetimeProp;
+    }
+
+    /**
+     * @param mixed $datetimeProp
+     */
+    public function setDatetimeProp(\DateTime $datetimeProp): self
+    {
+        $this->datetimeProp = $datetimeProp;
+
+        return $this;
+    }
+}
