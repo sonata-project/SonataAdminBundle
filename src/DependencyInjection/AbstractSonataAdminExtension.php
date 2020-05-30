648179c205b347d13053992dc04642da7ca1497d
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2020-05-31 00:52:54 +0200

    Expose field type constant (#6123)

--- src/DependencyInjection/AbstractSonataAdminExtension.php
+++ src/DependencyInjection/AbstractSonataAdminExtension.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Sonata\AdminBundle\DependencyInjection;
 
+use Sonata\AdminBundle\Templating\TemplateRegistry;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -33,54 +34,64 @@ abstract class AbstractSonataAdminExtension extends Extension
             'templates' => [
                 'types' => [
                     'list' => [
-                        'array' => '@SonataAdmin/CRUD/list_array.html.twig',
-                        'boolean' => '@SonataAdmin/CRUD/list_boolean.html.twig',
-                        'date' => '@SonataAdmin/CRUD/list_date.html.twig',
-                        'time' => '@SonataAdmin/CRUD/list_time.html.twig',
-                        'datetime' => '@SonataAdmin/CRUD/list_datetime.html.twig',
-                        'text' => '@SonataAdmin/CRUD/list_string.html.twig',
-                        'textarea' => '@SonataAdmin/CRUD/list_string.html.twig',
-                        'email' => '@SonataAdmin/CRUD/list_email.html.twig',
-                        'trans' => '@SonataAdmin/CRUD/list_trans.html.twig',
-                        'string' => '@SonataAdmin/CRUD/list_string.html.twig',
-                        'smallint' => '@SonataAdmin/CRUD/list_string.html.twig',
-                        'bigint' => '@SonataAdmin/CRUD/list_string.html.twig',
-                        'integer' => '@SonataAdmin/CRUD/list_string.html.twig',
-                        'decimal' => '@SonataAdmin/CRUD/list_string.html.twig',
-                        'identifier' => '@SonataAdmin/CRUD/list_string.html.twig',
-                        'currency' => '@SonataAdmin/CRUD/list_currency.html.twig',
-                        'percent' => '@SonataAdmin/CRUD/list_percent.html.twig',
-                        'choice' => '@SonataAdmin/CRUD/list_choice.html.twig',
-                        'url' => '@SonataAdmin/CRUD/list_url.html.twig',
-                        'html' => '@SonataAdmin/CRUD/list_html.html.twig',
-                        'many_to_many' => '@SonataAdmin/CRUD/Association/list_many_to_many.html.twig',
-                        'many_to_one' => '@SonataAdmin/CRUD/Association/list_many_to_one.html.twig',
-                        'one_to_many' => '@SonataAdmin/CRUD/Association/list_one_to_many.html.twig',
-                        'one_to_one' => '@SonataAdmin/CRUD/Association/list_one_to_one.html.twig',
+                        TemplateRegistry::TYPE_ARRAY => '@SonataAdmin/CRUD/list_array.html.twig',
+                        TemplateRegistry::TYPE_BOOLEAN => '@SonataAdmin/CRUD/list_boolean.html.twig',
+                        TemplateRegistry::TYPE_DATE => '@SonataAdmin/CRUD/list_date.html.twig',
+                        TemplateRegistry::TYPE_TIME => '@SonataAdmin/CRUD/list_time.html.twig',
+                        TemplateRegistry::TYPE_DATETIME => '@SonataAdmin/CRUD/list_datetime.html.twig',
+                        /* NEXT_MAJOR: Remove this line. */
+                        TemplateRegistry::TYPE_TEXT => '@SonataAdmin/CRUD/list_string.html.twig',
+                        TemplateRegistry::TYPE_TEXTAREA => '@SonataAdmin/CRUD/list_string.html.twig',
+                        TemplateRegistry::TYPE_EMAIL => '@SonataAdmin/CRUD/list_email.html.twig',
+                        TemplateRegistry::TYPE_TRANS => '@SonataAdmin/CRUD/list_trans.html.twig',
+                        TemplateRegistry::TYPE_STRING => '@SonataAdmin/CRUD/list_string.html.twig',
+                        /* NEXT_MAJOR: Remove this line. */
+                        TemplateRegistry::TYPE_SMALLINT => '@SonataAdmin/CRUD/list_string.html.twig',
+                        /* NEXT_MAJOR: Remove this line. */
+                        TemplateRegistry::TYPE_BIGINT => '@SonataAdmin/CRUD/list_string.html.twig',
+                        TemplateRegistry::TYPE_INTEGER => '@SonataAdmin/CRUD/list_string.html.twig',
+                        /* NEXT_MAJOR: Remove this line. */
+                        TemplateRegistry::TYPE_DECIMAL => '@SonataAdmin/CRUD/list_string.html.twig',
+                        TemplateRegistry::TYPE_FLOAT => '@SonataAdmin/CRUD/list_string.html.twig',
+                        TemplateRegistry::TYPE_IDENTIFIER => '@SonataAdmin/CRUD/list_string.html.twig',
+                        TemplateRegistry::TYPE_CURRENCY => '@SonataAdmin/CRUD/list_currency.html.twig',
+                        TemplateRegistry::TYPE_PERCENT => '@SonataAdmin/CRUD/list_percent.html.twig',
+                        TemplateRegistry::TYPE_CHOICE => '@SonataAdmin/CRUD/list_choice.html.twig',
+                        TemplateRegistry::TYPE_URL => '@SonataAdmin/CRUD/list_url.html.twig',
+                        TemplateRegistry::TYPE_HTML => '@SonataAdmin/CRUD/list_html.html.twig',
+                        TemplateRegistry::TYPE_MANY_TO_MANY => '@SonataAdmin/CRUD/Association/list_many_to_many.html.twig',
+                        TemplateRegistry::TYPE_MANY_TO_ONE => '@SonataAdmin/CRUD/Association/list_many_to_one.html.twig',
+                        TemplateRegistry::TYPE_ONE_TO_MANY => '@SonataAdmin/CRUD/Association/list_one_to_many.html.twig',
+                        TemplateRegistry::TYPE_ONE_TO_ONE => '@SonataAdmin/CRUD/Association/list_one_to_one.html.twig',
                     ],
                     'show' => [
-                        'array' => '@SonataAdmin/CRUD/show_array.html.twig',
-                        'boolean' => '@SonataAdmin/CRUD/show_boolean.html.twig',
-                        'date' => '@SonataAdmin/CRUD/show_date.html.twig',
-                        'time' => '@SonataAdmin/CRUD/show_time.html.twig',
-                        'datetime' => '@SonataAdmin/CRUD/show_datetime.html.twig',
-                        'text' => '@SonataAdmin/CRUD/base_show_field.html.twig',
-                        'email' => '@SonataAdmin/CRUD/show_email.html.twig',
-                        'trans' => '@SonataAdmin/CRUD/show_trans.html.twig',
-                        'string' => '@SonataAdmin/CRUD/base_show_field.html.twig',
-                        'smallint' => '@SonataAdmin/CRUD/base_show_field.html.twig',
-                        'bigint' => '@SonataAdmin/CRUD/base_show_field.html.twig',
-                        'integer' => '@SonataAdmin/CRUD/base_show_field.html.twig',
-                        'decimal' => '@SonataAdmin/CRUD/base_show_field.html.twig',
-                        'currency' => '@SonataAdmin/CRUD/show_currency.html.twig',
-                        'percent' => '@SonataAdmin/CRUD/show_percent.html.twig',
-                        'choice' => '@SonataAdmin/CRUD/show_choice.html.twig',
-                        'url' => '@SonataAdmin/CRUD/show_url.html.twig',
-                        'html' => '@SonataAdmin/CRUD/show_html.html.twig',
-                        'many_to_many' => '@SonataAdmin/CRUD/Association/show_many_to_many.html.twig',
-                        'many_to_one' => '@SonataAdmin/CRUD/Association/show_many_to_one.html.twig',
-                        'one_to_many' => '@SonataAdmin/CRUD/Association/show_one_to_many.html.twig',
-                        'one_to_one' => '@SonataAdmin/CRUD/Association/show_one_to_one.html.twig',
+                        TemplateRegistry::TYPE_ARRAY => '@SonataAdmin/CRUD/show_array.html.twig',
+                        TemplateRegistry::TYPE_BOOLEAN => '@SonataAdmin/CRUD/show_boolean.html.twig',
+                        TemplateRegistry::TYPE_DATE => '@SonataAdmin/CRUD/show_date.html.twig',
+                        TemplateRegistry::TYPE_TIME => '@SonataAdmin/CRUD/show_time.html.twig',
+                        TemplateRegistry::TYPE_DATETIME => '@SonataAdmin/CRUD/show_datetime.html.twig',
+                        /* NEXT_MAJOR: Remove this line. */
+                        TemplateRegistry::TYPE_TEXT => '@SonataAdmin/CRUD/base_show_field.html.twig',
+                        TemplateRegistry::TYPE_EMAIL => '@SonataAdmin/CRUD/show_email.html.twig',
+                        TemplateRegistry::TYPE_TRANS => '@SonataAdmin/CRUD/show_trans.html.twig',
+                        TemplateRegistry::TYPE_STRING => '@SonataAdmin/CRUD/base_show_field.html.twig',
+                        /* NEXT_MAJOR: Remove this line. */
+                        TemplateRegistry::TYPE_SMALLINT => '@SonataAdmin/CRUD/base_show_field.html.twig',
+                        /* NEXT_MAJOR: Remove this line. */
+                        TemplateRegistry::TYPE_BIGINT => '@SonataAdmin/CRUD/base_show_field.html.twig',
+                        TemplateRegistry::TYPE_INTEGER => '@SonataAdmin/CRUD/base_show_field.html.twig',
+                        /* NEXT_MAJOR: Remove this line. */
+                        TemplateRegistry::TYPE_DECIMAL => '@SonataAdmin/CRUD/base_show_field.html.twig',
+                        TemplateRegistry::TYPE_FLOAT => '@SonataAdmin/CRUD/base_show_field.html.twig',
+                        TemplateRegistry::TYPE_CURRENCY => '@SonataAdmin/CRUD/show_currency.html.twig',
+                        TemplateRegistry::TYPE_PERCENT => '@SonataAdmin/CRUD/show_percent.html.twig',
+                        TemplateRegistry::TYPE_CHOICE => '@SonataAdmin/CRUD/show_choice.html.twig',
+                        TemplateRegistry::TYPE_URL => '@SonataAdmin/CRUD/show_url.html.twig',
+                        TemplateRegistry::TYPE_HTML => '@SonataAdmin/CRUD/show_html.html.twig',
+                        TemplateRegistry::TYPE_MANY_TO_MANY => '@SonataAdmin/CRUD/Association/show_many_to_many.html.twig',
+                        TemplateRegistry::TYPE_MANY_TO_ONE => '@SonataAdmin/CRUD/Association/show_many_to_one.html.twig',
+                        TemplateRegistry::TYPE_ONE_TO_MANY => '@SonataAdmin/CRUD/Association/show_one_to_many.html.twig',
+                        TemplateRegistry::TYPE_ONE_TO_ONE => '@SonataAdmin/CRUD/Association/show_one_to_one.html.twig',
                     ],
                 ],
             ],
@@ -90,25 +101,33 @@ abstract class AbstractSonataAdminExtension extends Extension
         $bundles = $container->getParameter('kernel.bundles');
         if (isset($bundles['SonataIntlBundle'])) {
             $defaultConfig['templates']['types']['list'] = array_merge($defaultConfig['templates']['types']['list'], [
-                'date' => '@SonataIntl/CRUD/list_date.html.twig',
-                'datetime' => '@SonataIntl/CRUD/list_datetime.html.twig',
-                'smallint' => '@SonataIntl/CRUD/list_decimal.html.twig',
-                'bigint' => '@SonataIntl/CRUD/list_decimal.html.twig',
-                'integer' => '@SonataIntl/CRUD/list_decimal.html.twig',
-                'decimal' => '@SonataIntl/CRUD/list_decimal.html.twig',
-                'currency' => '@SonataIntl/CRUD/list_currency.html.twig',
-                'percent' => '@SonataIntl/CRUD/list_percent.html.twig',
+                TemplateRegistry::TYPE_DATE => '@SonataIntl/CRUD/list_date.html.twig',
+                TemplateRegistry::TYPE_DATETIME => '@SonataIntl/CRUD/list_datetime.html.twig',
+                /* NEXT_MAJOR: Remove this line. */
+                TemplateRegistry::TYPE_SMALLINT => '@SonataIntl/CRUD/list_decimal.html.twig',
+                /* NEXT_MAJOR: Remove this line. */
+                TemplateRegistry::TYPE_BIGINT => '@SonataIntl/CRUD/list_decimal.html.twig',
+                TemplateRegistry::TYPE_INTEGER => '@SonataIntl/CRUD/list_decimal.html.twig',
+                /* NEXT_MAJOR: Remove this line. */
+                TemplateRegistry::TYPE_DECIMAL => '@SonataIntl/CRUD/list_decimal.html.twig',
+                TemplateRegistry::TYPE_FLOAT => '@SonataIntl/CRUD/list_decimal.html.twig',
+                TemplateRegistry::TYPE_CURRENCY => '@SonataIntl/CRUD/list_currency.html.twig',
+                TemplateRegistry::TYPE_PERCENT => '@SonataIntl/CRUD/list_percent.html.twig',
             ]);
 
             $defaultConfig['templates']['types']['show'] = array_merge($defaultConfig['templates']['types']['show'], [
-                'date' => '@SonataIntl/CRUD/show_date.html.twig',
-                'datetime' => '@SonataIntl/CRUD/show_datetime.html.twig',
-                'smallint' => '@SonataIntl/CRUD/show_decimal.html.twig',
-                'bigint' => '@SonataIntl/CRUD/show_decimal.html.twig',
-                'integer' => '@SonataIntl/CRUD/show_decimal.html.twig',
-                'decimal' => '@SonataIntl/CRUD/show_decimal.html.twig',
-                'currency' => '@SonataIntl/CRUD/show_currency.html.twig',
-                'percent' => '@SonataIntl/CRUD/show_percent.html.twig',
+                TemplateRegistry::TYPE_DATE => '@SonataIntl/CRUD/show_date.html.twig',
+                TemplateRegistry::TYPE_DATETIME => '@SonataIntl/CRUD/show_datetime.html.twig',
+                /* NEXT_MAJOR: Remove this line. */
+                TemplateRegistry::TYPE_SMALLINT => '@SonataIntl/CRUD/show_decimal.html.twig',
+                /* NEXT_MAJOR: Remove this line. */
+                TemplateRegistry::TYPE_BIGINT => '@SonataIntl/CRUD/show_decimal.html.twig',
+                TemplateRegistry::TYPE_INTEGER => '@SonataIntl/CRUD/show_decimal.html.twig',
+                /* NEXT_MAJOR: Remove this line. */
+                TemplateRegistry::TYPE_DECIMAL => '@SonataIntl/CRUD/show_decimal.html.twig',
+                TemplateRegistry::TYPE_FLOAT => '@SonataIntl/CRUD/list_decimal.html.twig',
+                TemplateRegistry::TYPE_CURRENCY => '@SonataIntl/CRUD/show_currency.html.twig',
+                TemplateRegistry::TYPE_PERCENT => '@SonataIntl/CRUD/show_percent.html.twig',
             ]);
         }
 
