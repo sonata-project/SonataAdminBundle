52d4f72c9dccfd4794ab92ce31c89bd68f218d0a
Norbert Glanc <norglanc@gmail.com>
2020-07-15 11:29:59 +0200

    Remove duplicate import SonataFormBundle

--- tests/App/AppKernel.php
+++ tests/App/AppKernel.php
@@ -17,7 +17,6 @@ use Knp\Bundle\MenuBundle\KnpMenuBundle;
 use Sonata\AdminBundle\SonataAdminBundle;
 use Sonata\BlockBundle\SonataBlockBundle;
 use Sonata\Doctrine\Bridge\Symfony\Bundle\SonataDoctrineBundle;
-use Sonata\Form\Bridge\Symfony\Bundle\SonataFormBundle;
 use Sonata\Form\Bridge\Symfony\SonataFormBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
