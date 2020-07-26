627ade6d1aa24d71c27b62d1197c8e473c26d4f5
SonataCI <thomas+ci@sonata-project.org>
2020-07-26 09:11:51 +0000

    DevKit updates

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -9,5 +9,4 @@ echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
 if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
-if [ "$SONATA_CORE" != "" ]; then composer require "sonata-project/core-bundle:$SONATA_CORE" --no-update; fi;
 if [ "$SONATA_BLOCK" != "" ]; then composer require "sonata-project/block-bundle:$SONATA_BLOCK" --no-update; fi;
