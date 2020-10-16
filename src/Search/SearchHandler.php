ae2febd63cd648a7c750632752f89cb03d74b3bd
ivan <webwave011@gmail.com>
2020-10-16 22:33:45 +0200

    remove deprecated pool property

--- src/Search/SearchHandler.php
+++ src/Search/SearchHandler.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace Sonata\AdminBundle\Search;
 
 use Sonata\AdminBundle\Admin\AdminInterface;
-use Sonata\AdminBundle\Admin\Pool;
 use Sonata\AdminBundle\Datagrid\PagerInterface;
 use Sonata\AdminBundle\Filter\FilterInterface;
 
@@ -26,35 +25,13 @@ use Sonata\AdminBundle\Filter\FilterInterface;
 class SearchHandler
 {
     /**
-     * @var Pool|null
-     */
-    protected $pool;
-
-    /**
      * @var bool
      */
     private $caseSensitive;
 
-    /**
-     * NEXT_MAJOR: Change signature to __construct(bool $caseSensitive) and remove pool property.
-     *
-     * @param Pool|bool $deprecatedPoolOrCaseSensitive
-     */
-    public function __construct($deprecatedPoolOrCaseSensitive, bool $caseSensitive = true)
+    public function __construct(bool $caseSensitive = true)
     {
-        if ($deprecatedPoolOrCaseSensitive instanceof Pool) {
-            @trigger_error(sprintf(
-                'Passing %s as argument 1 to %s() is deprecated since sonata-project/admin-bundle 3.74.'
-                    .' It will accept only bool in version 4.0.',
-                Pool::class,
-                __METHOD__
-            ), E_USER_DEPRECATED);
-
-            $this->pool = $deprecatedPoolOrCaseSensitive;
-            $this->caseSensitive = $caseSensitive;
-        } else {
-            $this->caseSensitive = $deprecatedPoolOrCaseSensitive;
-        }
+        $this->caseSensitive = $caseSensitive;
     }
 
     /**
