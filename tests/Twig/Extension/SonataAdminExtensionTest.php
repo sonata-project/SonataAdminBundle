a24d75161390ddde8392aed5af13b137efe97223
Sullivan SENECHAL <soullivaneuh@gmail.com>
2020-12-08 15:25:32 +0100

    Applied fixes from FlintCI

--- tests/Twig/Extension/SonataAdminExtensionTest.php
+++ tests/Twig/Extension/SonataAdminExtensionTest.php
@@ -2706,7 +2706,7 @@ EOT
         $this->assertSame(1234567, $this->twigExtension->getUrlSafeIdentifier($model));
     }
 
-    public function testGetUrlsafeIdentifier_GivenAdmin_Foo(): void
+    public function testGetUrlsafeIdentifierGivenAdminFoo(): void
     {
         $model = new \stdClass();
 
@@ -2731,7 +2731,7 @@ EOT
         $this->assertSame(1234567, $this->twigExtension->getUrlSafeIdentifier($model, $this->admin));
     }
 
-    public function testGetUrlsafeIdentifier_GivenAdmin_Bar(): void
+    public function testGetUrlsafeIdentifierGivenAdminBar(): void
     {
         $model = new \stdClass();
 
