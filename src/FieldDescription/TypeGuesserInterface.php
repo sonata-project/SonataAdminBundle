10125795c127470a60b3b4599518c5b43c0f92f8
Fran Moreno <franmomu@gmail.com>
2021-02-16 01:55:07 +0100

    Introduce TypeGuesserInterface
    This will guess the field description type from its properties.

---
+++ src/FieldDescription/TypeGuesserInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\AdminBundle\FieldDescription;
+
+use Sonata\AdminBundle\Admin\FieldDescriptionInterface;
+use Symfony\Component\Form\Guess\TypeGuess;
+
+interface TypeGuesserInterface
+{
+    public function guess(FieldDescriptionInterface $fieldDescription): TypeGuess;
+}
