4fd22fddc2cd56ec965b919b161024af0f89dfd8
Vincent Langlet <misterdeviling@gmail.com>
2021-03-13 20:47:10 +0100

    Add extension point for dashboardButtons

--- src/Admin/AdminExtensionInterface.php
+++ src/Admin/AdminExtensionInterface.php
@@ -29,6 +29,7 @@ use Sonata\Form\Validator\ErrorElement;
  * @method array configureBatchActions(AdminInterface $admin, array $actions)
  * @method array configureExportFields(AdminInterface $admin, array $fields)
  * @method array configureActionButtons(AdminInterface $admin, array $list, string $action, object $object)
+ * @method array configureDashboardButtons(AdminInterface $admin, array $actions)
  * @method void  configureDefaultFilterValues(AdminInterface $admin, array &$filterValues)
  * @method void  configureDefaultSortValues(AdminInterface $admin, array &$sortValues)
  * @method void  configureFormOptions(AdminInterface $admin, array &$formOptions)
@@ -271,6 +272,12 @@ interface AdminExtensionInterface
      */
     // NEXT_MAJOR: Uncomment this method
     // public function configureActionButtons(AdminInterface $admin, array $list, string $action, object $object): array;
+
+    /*
+     * @phpstan-param AdminInterface<T> $admin
+     */
+    // NEXT_MAJOR: Uncomment this method
+    // public function configureDashboardButtons(AdminInterface $admin, array $actions): array;
 
     /*
      * NEXT_MAJOR: Uncomment this method
