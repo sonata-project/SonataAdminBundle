72eaf86893f6fc52e8d23480be791fb189f49f6b
Jordi <jordism91@gmail.com>
2021-04-20 08:26:52 +0200

    Unify js files

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+// any CSS you require will output into a single css file (app.css in this case)
+import '../scss/app.scss';
+
+// require jQuery normally
+import $ from 'jquery';
+
+// create global $ and jQuery variables
+// eslint-disable-next-line
+global.$ = global.jQuery = $;
+
+import 'jquery.scrollto';
+import 'jquery-ui/ui/widget';
+import 'jquery-ui/ui/widgets/sortable';
+import 'bootstrap';
+import 'moment';
+import '../vendor/bootstrap-datetimepicker.min';
+import 'jquery-form';
+import 'x-editable/dist/bootstrap3-editable/js/bootstrap-editable';
+import 'select2/dist/js/select2.full';
+import 'admin-lte';
+import 'icheck';
+import 'jquery-slimscroll';
+import 'waypoints/lib/jquery.waypoints';
+import 'waypoints/lib/shortcuts/sticky';
+import 'readmore-js';
+import 'masonry-layout';
