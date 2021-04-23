ec5cbf17a287c952fc4fa86997803fc59980b95d
Jordi <jordism91@gmail.com>
2021-04-23 16:11:21 +0200

    Improve explanations of imports

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,38 @@
-// any CSS you require will output into a single css file (app.css in this case)
+// Any SCSS/CSS you require will output into a single css file (app.css in this case)
 import '../scss/app.scss';
 
-// require jQuery normally
+// Require jQuery normally
 import $ from 'jquery';
 
-// create global $ and jQuery variables
+// Create global $ and jQuery variables to be used outside this script
 // eslint-disable-next-line
 global.$ = global.jQuery = $;
 
 import 'jquery.scrollto';
+
+// Only using sortable widget from jquery-ui library
 import 'jquery-ui/ui/widget';
 import 'jquery-ui/ui/widgets/sortable';
 import 'bootstrap';
 import 'moment';
+
+// Eonasdan Bootstrap DateTimePicker in its version 3 does not
+// provide the scss or plain css, it only provides the less version
+// of its source files, that's why it is not included it via npm.
 import '../vendor/bootstrap-datetimepicker.min';
 import 'jquery-form';
+
+// Boostrap 3 JavaScript for the x-editable library
 import 'x-editable/dist/bootstrap3-editable/js/bootstrap-editable';
+
+// Full version of the select2, needed because SonataAdmin needs
+// compat dropdownCss and it only comes on the full version
 import 'select2/dist/js/select2.full';
 import 'admin-lte';
 import 'icheck';
 import 'jquery-slimscroll';
+
+// jQuery waypoints version and sticky shortcut
 import 'waypoints/lib/jquery.waypoints';
 import 'waypoints/lib/shortcuts/sticky';
 import 'readmore-js';
