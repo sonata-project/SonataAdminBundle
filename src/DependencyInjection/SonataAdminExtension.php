199e3a2ae630dd111766ff476e4c4666d9367f31
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-08-29 18:33:45 +0200

    Fail fast if the service is missing

--- src/DependencyInjection/SonataAdminExtension.php
+++ src/DependencyInjection/SonataAdminExtension.php
@@ -137,6 +137,13 @@ final class SonataAdminExtension extends Extension
 
                 break;
             case 'sonata.admin.security.handler.acl':
+                if (!$container->has('security.acl.provider')) {
+                    throw new \RuntimeException(
+                        'The "security.acl.provider" service is needed to use ACL as security handler.'
+                        .' You MUST install and enable the "symfony/acl-bundle" bundle.'
+                    );
+                }
+
                 if (0 === \count($config['security']['information'])) {
                     $config['security']['information'] = [
                         'GUEST' => ['VIEW', 'LIST'],
