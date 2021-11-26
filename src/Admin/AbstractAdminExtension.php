29b681169d02bada8e35505ba50b6ab88f64e034
Jochem Klaver <7ochem@users.noreply.github.com>
2021-11-26 14:03:38 +0100

    Process NEXT_MAJOR hints for "Add pre batch action event" (#7620)
    A pre batch action event was added to 4.x through #7579. This
    processes all the NEXT_MAJOR comments for 5.x for this specific
    feature

--- src/Admin/AbstractAdminExtension.php
+++ src/Admin/AbstractAdminExtension.php
@@ -96,11 +96,6 @@ abstract class AbstractAdminExtension implements AdminExtensionInterface
         return $actions;
     }
 
-    // NEXT_MAJOR: Remove the PHPDoc block as the interface will then specify the types
-    /**
-     * @param mixed[] $idx
-     * @phpstan-param AdminInterface<T> $admin
-     */
     public function preBatchAction(AdminInterface $admin, string $actionName, ProxyQueryInterface $query, array &$idx, bool $allElements): void
     {
     }
