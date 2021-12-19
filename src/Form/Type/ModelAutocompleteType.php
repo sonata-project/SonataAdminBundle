464cf6327a4fca843b22f29ca29df02ef5e92b28
George-Cristian TUDORAN <george.tudoran@zitec.com>
2021-12-19 16:49:57 +0200

    Updated deprecated option check

--- src/Form/Type/ModelAutocompleteType.php
+++ src/Form/Type/ModelAutocompleteType.php
@@ -163,7 +163,7 @@ final class ModelAutocompleteType extends AbstractType
             ...$this->deprecationParameters(
                 '4.x',
                 static function (Options $options, $value): string {
-                    if(!is_null($value)) {
+                    if($value !== 100) {
                         return 'Passing a value to option "quiet_millis" is deprecated! Use "delay" instead!';
                     }
                 }
