73406fc2061a097664523d113b901b4dad80771c
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2022-02-25 13:10:49 +0100

    Update src/Form/Type/TemplateType.php
    Co-authored-by: Christian Gripp <core23@users.noreply.github.com>

--- src/Form/Type/TemplateType.php
+++ src/Form/Type/TemplateType.php
@@ -19,7 +19,7 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TemplateType extends AbstractType
+final class TemplateType extends AbstractType
 {
     /**
      * @param array<string, mixed> $options
