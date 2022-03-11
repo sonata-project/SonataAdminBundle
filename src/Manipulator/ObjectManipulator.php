80c12663b1d8e53eab082eb7ea97871cbdbad707
Vincent Langlet <vincentlanglet@hotmail.fr>
2022-03-11 21:49:28 +0100

    Refacto

--- src/Manipulator/ObjectManipulator.php
+++ src/Manipulator/ObjectManipulator.php
@@ -13,11 +13,8 @@ declare(strict_types=1);
 
 namespace Sonata\AdminBundle\Manipulator;
 
-use Doctrine\Common\Util\ClassUtils;
-use Doctrine\Inflector\InflectorFactory;
 use Sonata\AdminBundle\FieldDescription\FieldDescriptionInterface;
 use Symfony\Component\PropertyAccess\PropertyAccess;
-use Symfony\Component\PropertyAccess\PropertyAccessor;
 
 final class ObjectManipulator
 {
@@ -36,16 +33,23 @@ final class ObjectManipulator
         $associationMapping = $parentFieldDescription->getAssociationMapping();
         $parentAssociationMappings = $parentFieldDescription->getParentAssociationMappings();
 
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
+
         foreach ($parentAssociationMappings as $parentAssociationMapping) {
             $fieldName = $parentAssociationMapping['fieldName'];
             \assert(\is_string($fieldName));
-            $object = self::callGetter($object, $fieldName);
+
+            $object = $propertyAccessor->getValue($object, $fieldName);
         }
 
         $fieldName = $associationMapping['fieldName'];
         \assert(\is_string($fieldName));
 
-        return self::callAdder($object, $instance, $fieldName);
+        $collection = $propertyAccessor->getValue($object, $fieldName);
+        $collection[] = $instance;
+        $propertyAccessor->setValue($object, $fieldName, $collection);
+
+        return $instance;
     }
 
     /**
@@ -67,55 +71,16 @@ final class ObjectManipulator
 
         $parentAssociationMappings = $parentFieldDescription->getParentAssociationMappings();
 
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
+
         foreach ($parentAssociationMappings as $parentAssociationMapping) {
             $fieldName = $parentAssociationMapping['fieldName'];
             \assert(\is_string($fieldName));
-            $object = self::callGetter($object, $fieldName);
+
+            $object = $propertyAccessor->getValue($object, $fieldName);
         }
 
-        return self::callSetter($instance, $object, $mappedBy);
-    }
-
-    /**
-     * Call $object->getXXX().
-     */
-    private static function callGetter(object $object, string $fieldName): object
-    {
-        $propertyAccessor = PropertyAccess::createPropertyAccessor();
-
-        return $propertyAccessor->getValue($object, $fieldName);
-    }
-
-    /**
-     * Call $instance->setXXX($object).
-     *
-     * @phpstan-template T of object
-     * @phpstan-param T $instance
-     * @phpstan-return T
-     */
-    private static function callSetter(object $instance, object $object, string $mappedBy): object
-    {
-        $propertyAccessor = PropertyAccess::createPropertyAccessor();
-
         $propertyAccessor->setValue($instance, $mappedBy, $object);
-
-        return $instance;
-    }
-
-    /**
-     * Call $object->addXXX($instance).
-     *
-     * @phpstan-template T of object
-     * @phpstan-param T $instance
-     * @phpstan-return T
-     */
-    private static function callAdder(object $object, object $instance, string $fieldName): object
-    {
-        $propertyAccessor = PropertyAccess::createPropertyAccessor();
-
-        $collection = $propertyAccessor->getValue($object, $fieldName);
-        $collection[] = $instance;
-        $propertyAccessor->setValue($object, $fieldName, $collection);
 
         return $instance;
     }
