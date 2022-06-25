f79270e18dc3974266e48300ec640227b6e029ac
Jordi Sala Morales <jordism91@gmail.com>
2022-06-25 13:16:31 +0200

    Improve lazyness of commands

--- src/Resources/config/commands.php
+++ src/Resources/config/commands.php
@@ -20,30 +20,29 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigura
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     // Use "service" function for creating references to services when dropping support for Symfony 4.4
-    // Use "param" function for creating references to parameters when dropping support for Symfony 5.1
     $containerConfigurator->services()
 
         ->set('sonata.admin.command.explain', ExplainAdminCommand::class)
-            ->tag('console.command', ['command' => 'sonata:admin:explain'])
+            ->tag('console.command')
             ->args([
                 new ReferenceConfigurator('sonata.admin.pool'),
             ])
 
         ->set('sonata.admin.command.generate_object_acl', GenerateObjectAclCommand::class)
-            ->tag('console.command', ['command' => 'sonata:admin:generate-object-acl'])
+            ->tag('console.command')
             ->args([
                 new ReferenceConfigurator('sonata.admin.pool'),
                 [],
             ])
 
         ->set('sonata.admin.command.list', ListAdminCommand::class)
-            ->tag('console.command', ['command' => 'sonata:admin:list'])
+            ->tag('console.command')
             ->args([
                 new ReferenceConfigurator('sonata.admin.pool'),
             ])
 
         ->set('sonata.admin.command.setup_acl', SetupAclCommand::class)
-            ->tag('console.command', ['command' => 'sonata:admin:setup-acl'])
+            ->tag('console.command')
             ->args([
                 new ReferenceConfigurator('sonata.admin.pool'),
                 new ReferenceConfigurator('sonata.admin.manipulator.acl.admin'),
