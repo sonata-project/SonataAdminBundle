4a16b5d002eba12851c5cd68c776b05da29bfd55
SonataCI <thomas+ci@sonata-project.org>
2023-03-05 12:12:23 +0000

    DevKit updates

--- Makefile
+++ Makefile
@@ -117,7 +117,7 @@ phpstan:
 .PHONY: phpstan
 
 psalm:
-	vendor/bin/psalm --php-version=8.1
+	vendor/bin/psalm --php-version=8.2
 .PHONY: psalm
 
 rector:
--- rector.php
+++ rector.php
@@ -29,7 +29,7 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_74,
+        LevelSetList::UP_TO_PHP_80,
     ]);
 
     $rectorConfig->importNames();
