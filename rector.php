3eb179fe049fb84bc203509f483d38aead029cf1
SonataCI <thomas+ci@sonata-project.org>
2024-12-07 18:22:41 +0000

    DevKit updates

--- rector.php
+++ rector.php
@@ -19,6 +19,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php70\Rector\FunctionLike\ExceptionHandlerTypehintRector;
+use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\NarrowUnusedSetUpDefinedPropertyRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
@@ -42,6 +43,7 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->skip([
         ExceptionHandlerTypehintRector::class,
         ReadOnlyPropertyRector::class,
+        NullToStrictStringFuncCallArgRector::class,
         PreferPHPUnitThisCallRector::class,
         NarrowUnusedSetUpDefinedPropertyRector::class,
     ]);
